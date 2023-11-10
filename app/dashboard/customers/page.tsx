deef92fdf2c029608c7c427309257f89755f4fde
pica69co <oscarwarrieta@gmail.com>
2023-11-10 17:54:07 -0500

    Add new dashboard pages and update layout and fonts

---
+++ app/dashboard/customers/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function page() {
+  return <p>Customer page</p>;
+}
+
+export default page;
---
+++ app/dashboard/invoices/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function page() {
+  return <p>Invoices page</p>;
+}
+
+export default page;
