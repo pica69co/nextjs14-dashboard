1112e74edb6a7bb8bb6b0f6b176e317756bfcee5
pica69co <oscarwarrieta@gmail.com>
2023-11-11 17:53:44 -0500

    Add not-found component and handle missing invoice

---
+++ app/dashboard/invoices/[id]/edit/not-found.tsx
@@ -0,0 +1,17 @@
+import { FaceFrownIcon } from "@heroicons/react/24/outline";
+import Link from "next/link";
+
+export default function Notfound() {
+  return (
+    <main className="flex h-full flex-col items-center justify-center gap-2">
+      <FaceFrownIcon className="w-10 text-gray-400" />
+      <h2 className="text-center">Something went wrong!</h2>
+      <Link
+        href="/dashboard/invoices"
+        className="mt-4 rounded-md bg-blue-500 px-4 py-2 text-sm text-white transition-colors hover:bg-blue-400"
+      >
+        Go Back
+      </Link>
+    </main>
+  );
+}
